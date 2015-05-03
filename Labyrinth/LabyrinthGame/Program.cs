4ab36a177eb8749bd0de7f45ae4d1f16c0f07b34
00dri <alexandrova.adriana@gmail.com>
2015-05-03 13:59:07 +0300

    Initial commit. Added from the Labyrinth-5 project. Added classes to be implemented.

---
+++ Labyrinth/LabyrinthGame/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabyrinthGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new Labyrinth();
+            test.PlayGame();
+        }
+    }
+}
