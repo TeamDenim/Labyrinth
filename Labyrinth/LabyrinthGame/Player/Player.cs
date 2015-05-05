03ec979a8ba9d0d9be59cdab5db8dae3db7d648b
00dri <alexandrova.adriana@gmail.com>
2015-05-06 01:55:33 +0300

    Refactored player Move method

--- Labyrinth/LabyrinthGame/Player/Player.cs
+++ Labyrinth/LabyrinthGame/Player/Player.cs
@@ -46,30 +46,47 @@
 
         public virtual void Move(int dirX, int dirY)
         {
-            if (this.IsMoveValid(this.currentPlayerPositionX + dirX, this.currentPlayerPositionY + dirY) == false)
+            if (this.IsMoveValid(this.currentPlayerPositionX + dirX, this.currentPlayerPositionY + dirY) )
             {
-                return;
+                if (IsBlockedCell( dirX, dirY))
+                {
+                    Console.WriteLine(Messages.INVALID_MOVE_MESSAGE);
+                }
+                else
+                {
+                    DrawPlayer(dirX, dirY);
+                    DrawFreeCell();
+                    ChangePlayerPositon(dirX, dirY);
+                }
             }
+        }
 
-            if (this.labyrinth[this.currentPlayerPositionY + dirY, this.currentPlayerPositionX + dirX] == LabyrinthConstants.BLOCKED_CELL_CHAR)
-            {
-                Console.WriteLine(Messages.INVALID_MOVE_MESSAGE);
-                return;
-            }
-            else
-            {
-                this.labyrinth[this.currentPlayerPositionY, this.currentPlayerPositionX] = LabyrinthConstants.FREE_CELL_CHAR;
-                this.labyrinth[this.currentPlayerPositionY + dirY, this.currentPlayerPositionX + dirX] = LabyrinthConstants.PLAYER_SIGN_CHAR;
-                this.currentPlayerPositionY += dirY;
-                this.currentPlayerPositionX += dirX;
-                return;
-            }
+        private void ChangePlayerPositon(int dirX, int dirY)
+        {
+            this.currentPlayerPositionY += dirY;
+            this.currentPlayerPositionX += dirX;
+        }
+
+        private bool IsBlockedCell(int dirX, int dirY)
+        {
+            return this.labyrinth[this.currentPlayerPositionY + dirY, this.currentPlayerPositionX + dirX] ==
+                   LabyrinthConstants.BLOCKED_CELL_CHAR;
         }
 
         public virtual bool IsMoveValid(int x, int y)
         {
             return x >= 0 && x <= LabyrinthConstants.LABYRINTH_SIZE - 1 
                 && y >= 0 && y <= LabyrinthConstants.LABYRINTH_SIZE - 1;
+        }
+
+        public char DrawPlayer(int dirX, int dirY)
+        {
+            return this.labyrinth[this.currentPlayerPositionY + dirY, this.currentPlayerPositionX + dirX] = LabyrinthConstants.PLAYER_SIGN_CHAR;
+        }
+
+        public char DrawFreeCell()
+        {
+            return this.labyrinth[this.currentPlayerPositionY, this.currentPlayerPositionX] = LabyrinthConstants.FREE_CELL_CHAR;
         }
     }
 }
