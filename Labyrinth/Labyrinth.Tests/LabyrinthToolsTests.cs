1f1e001aa456a67a50fbc1b7d870e379f469ffb7
DimitarStoyanovVictory <Dimitar.Stoyanov.Victory@gmail.com>
2015-05-06 15:01:30 +0300

    LabyrinthToolTest
    new test methods

--- Labyrinth/Labyrinth.Tests/LabyrinthToolsTests.cs
+++ Labyrinth/Labyrinth.Tests/LabyrinthToolsTests.cs
@@ -1,14 +1,33 @@
 ﻿namespace Labyrinth.Tests
 {
+    using Common.LabyrinthTools;
+    using Common.Constants;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
 
     [TestClass]
-    class LabyrinthToolsTests
+    public class LabyrinthToolsTests
     {
         [TestMethod]
         public void TestGenerateLabyrinth()
         {
+            LabyrinthTools labyrinth = new LabyrinthTools();
 
+            char[,] matix = labyrinth.GenerateLabyrinth();
+
+            bool wrongItem = false;
+            foreach (var item in matix)
+            {
+                if (item == LabyrinthConstants.BLOCKED_CELL_CHAR || item == LabyrinthConstants.FREE_CELL_CHAR ||
+                    item == LabyrinthConstants.PLAYER_SIGN_CHAR)
+                {
+                    Assert.IsTrue(wrongItem == false, "the labyrinth has the right items");
+                }
+                else
+                {
+                    wrongItem = true;
+                    Assert.IsTrue(wrongItem == true, "there is wrong item or items, pleace check labyrinth items again");
+                }
+            }
         }
 
         [TestMethod]
